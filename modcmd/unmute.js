165d14be43242fd100409474ea542bf944ec749f
mitimood <eliaskamel2011@gmail.com>
2021-08-02 00:02:45 -0300

    asd

--- modcmd/unmute.js
+++ modcmd/unmute.js
@@ -18,11 +18,11 @@ async function unmute(msg){
             if(member.voice){
                 member.voice.setMute(false)
             }
-            return msg.channel.send(msg.author.tag+` O membro ${member.user.tag} foi mutado` )
+            return msg.channel.send(msg.author+` O membro ${member.user} foi desmutado` )
         }else{
-            return msg.channel.send(msg.author.tag+" O membro não esta mutado")
+            return msg.channel.send(msg.author+" O membro não esta mutado")
         }
     }else{
-        return msg.channel.send(msg.author.tag+" O membro não esta no servidor")
+        return msg.channel.send(msg.author+" O membro não esta no servidor")
     }
 }
