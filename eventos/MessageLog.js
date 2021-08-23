f7dc042a0da96174bf7dca9cf45c20f73529d993
mitimood <eliaskamel2011@gmail.com>
2021-08-23 18:37:47 -0300

    id to message log

--- eventos/MessageLog.js
+++ eventos/MessageLog.js
@@ -8,8 +8,8 @@ client.on("messageUpdate", (oldMessage, newMessage)=>{
         let newEmb = new MessageEmbed()
         let oldEmb = new MessageEmbed()
 
-        oldEmb.setDescription("Mensagem antiga em: " + "<#" + oldMessage.channel.id + ">\n\n" + "```\n" + oldMessage.content + "\n```").setColor("GREY").setAuthor(oldMessage.author.username, oldMessage.author.avatarURL(), oldMessage.author.avatarURL()).setTimestamp(oldMessage.createdTimestamp).setTitle(oldMessage.channel.name )
-        newEmb.setDescription("Mensagem nova em: " + "<#" + newMessage.channel.id + ">\n\n" + "```\n" + newMessage.content + "\n```").setColor("GREEN").setAuthor(newMessage.author.username, newMessage.author.avatarURL(), newMessage.author.avatarURL()).setTimestamp(newMessage.createdTimestamp).setTitle(newMessage.channel.name )
+        oldEmb.setDescription("Mensagem antiga em: " + "<#" + oldMessage.channel.id + ">\n\n" + "```\n" + oldMessage.content + "\n```").setColor("GREY").setAuthor(oldMessage.author.username, oldMessage.author.avatarURL(), oldMessage.author.avatarURL()).setTimestamp(oldMessage.createdTimestamp).setTitle(oldMessage.channel.name ).setFooter(oldMessage.member.id)
+        newEmb.setDescription("Mensagem nova em: " + "<#" + newMessage.channel.id + ">\n\n" + "```\n" + newMessage.content + "\n```").setColor("GREEN").setAuthor(newMessage.author.username, newMessage.author.avatarURL(), newMessage.author.avatarURL()).setTimestamp(newMessage.createdTimestamp).setTitle(newMessage.channel.name ).setFooter(newMessage.member.id)
 
         newMessage.guild.channels.cache.get(config.channels.msglog).send({embeds:[oldEmb, newEmb]})
 
@@ -23,7 +23,7 @@ client.on("messageDelete", (delMessage)=>{
     try{
         let delemb = new MessageEmbed()
 
-        delemb.setDescription("Mensagem deletada em <#" + delMessage.channel.id + ">\n" + "```" + delMessage.content + "```").setColor("DARK_RED").setAuthor(delMessage.author.username, delMessage.author.avatarURL(), delMessage.author.avatarURL()).setTimestamp(delMessage.createdTimestamp).setTitle(delMessage.channel.name)
+        delemb.setDescription("Mensagem deletada em <#" + delMessage.channel.id + ">\n" + "```" + delMessage.content + "```").setColor("DARK_RED").setAuthor(delMessage.author.username, delMessage.author.avatarURL(), delMessage.author.avatarURL()).setTimestamp(delMessage.createdTimestamp).setTitle(delMessage.channel.name).setFooter(delMessage.member.id)
         
         delMessage.guild.channels.cache.get(config.channels.msglog).send({embeds:[delemb]})
     }catch(err){
