33cc13af9b3eaeb0e14adb6a457a6d32099eaf8e
mitimood <eliaskamel2011@gmail.com>
2021-08-28 11:28:11 -0300

    Auto filter bug corection

--- eventos/autofilter.js
+++ eventos/autofilter.js
@@ -6,7 +6,7 @@ Filters all the messages sent searching for scam links
 */
 
 client.on("messageCreate", async msg =>{
-    if (/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g.test(msg.content)&&msg.member.roles.cache.size == 1){
+    if (/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/g.test(msg.content) && msg?.member?.roles?.cache?.size == 1){
         msg.delete()
     }
 
