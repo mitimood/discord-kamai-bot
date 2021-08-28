9148b041dc0248405667246a0968bfa4811a1f47
mitimood <eliaskamel2011@gmail.com>
2021-08-28 11:28:36 -0300

    atachment support in message edit event

--- eventos/MessageLog.js
+++ eventos/MessageLog.js
@@ -8,8 +8,21 @@ client.on("messageUpdate", (oldMessage, newMessage)=>{
         let newEmb = new MessageEmbed()
         let oldEmb = new MessageEmbed()
 
-        oldEmb.setDescription("Mensagem antiga em: " + "<#" + oldMessage.channel.id + ">\n\n" + "```\n" + oldMessage.content + "\n```").setColor("GREY").setAuthor(oldMessage.author.username, oldMessage.author.avatarURL(), oldMessage.author.avatarURL()).setTimestamp(oldMessage.createdTimestamp).setTitle(oldMessage.channel.name ).setFooter(oldMessage.member.id)
-        newEmb.setDescription("Mensagem nova em: " + "<#" + newMessage.channel.id + ">\n\n" + "```\n" + newMessage.content + "\n```").setColor("GREEN").setAuthor(newMessage.author.username, newMessage.author.avatarURL(), newMessage.author.avatarURL()).setTimestamp(newMessage.createdTimestamp).setTitle(newMessage.channel.name ).setFooter(newMessage.member.id)
+        let oldImageUrl = '';
+
+        for( atach of oldMessage.attachments ){
+            oldImageUrl +=  `\n${atach[1].attachment}`
+        }
+
+        oldEmb.setDescription("Mensagem antiga em: " + "<#" + oldMessage.channel.id + ">\n\n" + "```\n" + oldMessage.content + "\n```" + oldImageUrl).setColor("GREY").setAuthor(oldMessage.author.username, oldMessage.author.avatarURL(), oldMessage.author.avatarURL()).setTimestamp(oldMessage.createdTimestamp).setTitle(oldMessage.channel.name ).setFooter(oldMessage.member.id)
+        
+        let newImageUrl = '';
+
+        for( atach of newMessage.attachments ){
+            newImageUrl +=  `\n${atach[1].attachment}`
+        }
+        
+        newEmb.setDescription("Mensagem nova em: " + "<#" + newMessage.channel.id + ">\n\n" + "```\n" + newMessage.content + "\n```" + newImageUrl).setColor("GREEN").setAuthor(newMessage.author.username, newMessage.author.avatarURL(), newMessage.author.avatarURL()).setTimestamp(newMessage.createdTimestamp).setTitle(newMessage.channel.name ).setFooter(newMessage.member.id)
 
         newMessage.guild.channels.cache.get(config.channels.msglog).send({embeds:[oldEmb, newEmb]})
 
@@ -19,13 +32,20 @@ client.on("messageUpdate", (oldMessage, newMessage)=>{
 })
 
 client.on("messageDelete", (delMessage)=>{
+
     if (delMessage.author.bot) return
     try{
         let delemb = new MessageEmbed()
+        let imageUrl = '';
 
-        delemb.setDescription("Mensagem deletada em <#" + delMessage.channel.id + ">\n" + "```" + delMessage.content + "```").setColor("DARK_RED").setAuthor(delMessage.author.username, delMessage.author.avatarURL(), delMessage.author.avatarURL()).setTimestamp(delMessage.createdTimestamp).setTitle(delMessage.channel.name).setFooter(delMessage.member.id)
+        for( atach of delMessage.attachments ){
+            imageUrl += `\n${atach[1].attachment}`
+        }
+
+        delemb.setDescription("Mensagem deletada em <#" + delMessage.channel.id + ">\n" + "```" + delMessage.content  +"```" + imageUrl).setColor("DARK_RED").setAuthor(delMessage.author.username, delMessage.author.avatarURL(), delMessage.author.avatarURL()).setTimestamp(delMessage.createdTimestamp).setTitle(delMessage.channel.name).setFooter(delMessage.member.id)
         
         delMessage.guild.channels.cache.get(config.channels.msglog).send({embeds:[delemb]})
+
     }catch(err){
         console.log(err)
     }
