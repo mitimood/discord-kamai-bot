f696a268b5a191c65940e53c222b1a5cf8cb7b43
mitimood <eliaskamel2011@gmail.com>
2021-10-18 23:02:46 -0300

    adiconando comando adicionar xp

---
+++ commands/admcmd/xpAdd.js
@@ -0,0 +1,31 @@
+const { TrimMsg } = require("../../funções/funções");
+const { add_voice_xp, add_chat_xp } = require("../../mongodb");
+
+/*
+    say command, will reply a message content inside the especified channel
+
+say (channel id) (message content)
+*/
+module.exports={
+    name: "xpadd",
+    aliases: [],
+    description: "Adiciona xp",
+
+    async execute (message){
+
+    
+    var msgArgs = TrimMsg(message)
+
+    if(msgArgs[1] == "voice"){
+        add_voice_xp(msgArgs[2], msgArgs[3])
+        message.channel.send(`DEI PARA ${msgArgs[2]} ${msgArgs[2]}XP VOICE POINTS`)
+
+    }else if( msgArgs[1] == "chat"){
+        add_chat_xp(msgArgs[2], msgArgs[3])
+        message.channel.send(`DEI PARA ${msgArgs[2]} ${msgArgs[2]}XP CHAT POINTS`)
+    }else{
+        message.channel.send("xpadd chat/voice id points")
+    }
+    
+    }
+}
