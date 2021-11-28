4ccc6325ec4da6b598ceb08a24816b8a65ccb313
mitimood <54547250+mitimood@users.noreply.github.com>
2021-11-28 19:55:53 -0300

    correção erro entrada rapida

--- eventos/memJoinCheck.js
+++ eventos/memJoinCheck.js
@@ -5,28 +5,34 @@ const { ban_member_send_message } = require("../funções/funções");
 
 // check some informations when a member join
 client.on("guildMemberAdd", async (member) => {
-    console.log(`Verificando entrada ` + new Date())
+    console.log(`Verificando entrada ` + Date.UTC())
     if(member.guild.id != config.guild_id) return
-    let userid=member.user.id
+    let userid = member.user.id
 
     let muted = await CheckMute(member.id)
-    if(muted)member.roles.add(config.roles.muted, "Mutado por entrar após tempmute")
+    try {
+        if(muted) await member.roles.add(config.roles.muted, "Mutado por entrar após tempmute")
 
-    let roles = await check_roles(member.id)
-    if(roles)member.roles.add(roles)
-
-    let warns = await warn_list(userid) 
+        let roles = await check_roles(member.id)
     
-            if (warns["points"] == 1)  member.roles.add(config.roles.adv1)
-            if (warns["points"] == 2)  member.roles.add([config.roles.adv1, config.roles.adv2])
-            if (warns["points"] == 3)  member.roles.add([config.roles.adv1, config.roles.adv2, config.roles.adv3])
+        if(roles) await member.roles.add(roles)
+    
+        let warns = await warn_list(userid) 
+        
+        if (warns["points"] == 1) await member.roles.add(config.roles.adv1)
+        if (warns["points"] == 2) await member.roles.add([config.roles.adv1, config.roles.adv2])
+        if (warns["points"] == 3) await member.roles.add([config.roles.adv1, config.roles.adv2, config.roles.adv3])
+    
+    } catch (error) {
+        console.log(error)
+    }
 
     
     const regTst = /milena[0-9]+|^! cd17z\W*\w*|Bruninhaa+|Amandaa+|Amandinhaa+|Larinhaa+|Thalitaa+|clarinhaa+|Plyss|! Baixinhaa*|Safiraa+|Mirelinha dos pack/ig
     if((member.displayName.toLowerCase()).match(regTst)){
         try{
             await ban_member_send_message(member.id,"Selfbot!!", member.guild, client.user)
-            selfbotRegister.selfbotAdd(new Date(), member.avatar, member.id, member.user.tag, member.user.createdTimestamp, member.joinedTimestamp)
+            selfbotRegister.selfbotAdd(Date.now().valueOf(), member.avatar, member.id, member.user.tag, member.user.createdTimestamp, member.joinedTimestamp)
         }catch(error){
             console.log(error)
         }
