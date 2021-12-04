30592275e2675469c3be3d745e22d022301e24cd
mitimood <54547250+mitimood@users.noreply.github.com>
2021-12-04 15:50:32 -0300

    retirando console

--- mongodb.js
+++ mongodb.js
@@ -700,8 +700,6 @@ async function addReport(id, toDo, authorId, messages){
 
 async function updateStateReport(id, state){
   try{
-    console.log(state)
-    console.log(id)
     const database = MongodbClient.db(config.mongo.db_geral);
     const report = database.collection('reports');
   
