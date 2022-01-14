c777b103d69e13a9094bd6dc7c157b401730adb2
mitimood <54547250+mitimood@users.noreply.github.com>
2022-01-13 21:46:17 -0300

    tratamento de erros api delay

--- commands/admcmd/list.js
+++ commands/admcmd/list.js
@@ -2,6 +2,7 @@ const { client } = require("../../");
 const config = require("../../config");
 const { TrimMsg } = require("../../utils/auxiliarFunctions");
 const mongoDB = require("../../mongodb");
+const { logger } = require("../../utils/logger");
 
 /*
 show a list of user warnings
@@ -15,16 +16,16 @@ module.exports={
     description: "Lista as advertencias de um usuário",
 
     async execute (msg){
-
+        try {
             let msgArgs = TrimMsg(msg)
-            if(!msgArgs[1] || !msgArgs[1].match(/[0-9]+/) && !msg.mentions.members.first())return msg.channel.send("Mencione um usuário")
+            if(!msgArgs[1] || !msgArgs[1].match(/[0-9]+/) && !msg.mentions.members.first())return await msg.channel.send("Mencione um usuário")
             let userid = (msg.mentions.members.first()) ? msg.mentions.members.first().user.id : msgArgs[1].match(/[0-9]+/)[0];
 
             let warns_list = await mongoDB.warn_list(userid)
             if(warns_list){
                 let user = await client.users.fetch(userid, true)
                 
-                msg.channel.send({embeds:[{
+                await msg.channel.send({embeds:[{
                     author:{name:user.tag, url: user.displayAvatarURL() },
                     description: warns_list["warns"],
                     footer:{
@@ -33,7 +34,11 @@ module.exports={
                     color: config.color.sucess
                 }]})
             }else{
-                return msg.channel.send("Não existe registro de advertencias para esse membro")
+                return await msg.channel.send("Não existe registro de advertencias para esse membro")
             }
+        } catch (error) {
+            logger.error(error)
+        }
+            
     }
 }
