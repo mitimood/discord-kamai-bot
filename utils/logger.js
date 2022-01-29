80a46ac79f97b78c445609c3333bec15accfd40e
mitimood <54547250+mitimood@users.noreply.github.com>
2022-01-29 14:02:39 -0300

    registro de erros

--- utils/logger.js
+++ utils/logger.js
@@ -1,23 +1,37 @@
 const winston = require('winston')
+const { format, transports } = winston
+const path = require('path')
 
-const date = Date.now()
+const logFormat = format.printf(info => `${info.timestamp} ${info.level} [${info.label}]: ${info.message}`)
+
+const date = new Date().valueOf()
 
 const logger = winston.createLogger({
-    format: winston.format.combine(
-        winston.format.timestamp({
-           format: 'MMM-DD-YYYY HH:mm:ss'
-       }),
-        winston.format.printf(info => `${info.level}:${[info.timestamp]}: ${info.message}`),
-    ),
-    transports: [
-        new winston.transports.File({ filename: `./utils/logs/${date} error.log`, level: 'error' }),
-        new winston.transports.File({ filename: `./utils/logs/${date} info.log`, level: 'info' }),
-    ],
-});
- 
-logger.add(new winston.transports.Console({
-    format: winston.format.simple()
-}));
+  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+  format: format.combine(
+    format.label({ label: path.basename(process.mainModule.filename) }),
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    // Format the metadata object
+    format.metadata({ fillExcept: ['message', 'level', 'timestamp', 'label'] })
+  ),
+  transports: [
+    new transports.Console({
+      format: format.combine(
+        format.colorize(),
+        logFormat
+      )
+    }),
+    new transports.File({
+      filename: `utils/logs/${date}.log`,
+      format: format.combine(
+        // Render in one line in your log file.
+        // If you use prettyPrint() here it will be really
+        // difficult to exploit your logs files afterwards.
+        format.prettyPrint()
+      )
+    })
+  ],
+  exitOnError: false
+})
 
-
-exports.logger = logger
+module.exports = logger
