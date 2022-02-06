d16c3e3196c09459ecb4df4da75b2a929ce4062f
mitimood <54547250+mitimood@users.noreply.github.com>
2022-02-06 19:05:40 -0300

    mudanças call premium

---
+++ eventos/premiumChannel.js
@@ -0,0 +1,55 @@
+const config = require("../config");
+const {client, LocalDb} = require(`../index`);
+const logger = require("../utils/logger");
+
+client.on("voiceStateUpdate", async (oldstate,newstate)=>
+{
+    try {
+
+        const premiumRoles = [config.roles.levels["100"], config.roles.nitro]
+
+        
+        const hasPremium = newstate.member.roles.cache.filter((r)=>premiumRoles.includes(r.id)).size >= 1
+
+        let channelcreate = newstate.guild.channels.cache.get(config.channels.pvtcreat);
+        var deletechan;
+        //Delete channel when empty
+        //Previous state category same category as the create category, member has role create, channel already created
+        if(( ( oldstate.channel && oldstate.channel.parent == channelcreate.parent ) && hasPremium) && await LocalDb.userVoiceExist(oldstate.id)){
+            let channelUser = oldstate.guild.channels.cache.get(await JSON.parse(JSON.stringify(await LocalDb.channelget(oldstate.id))).id)
+            if(channelUser != newstate.channel){
+                console.log(`Deletando canal premium` + new Date())
+                LocalDb.attactivity(newstate.id,0)
+                setTimeout(async() => {
+                    if(await LocalDb.verifyactivity(oldstate.id)==1) return
+                    await channelUser.delete().catch(err=>null)
+                    LocalDb.chandelete(newstate.id)
+                }, 3000);
+            }
+        }
+        
+        //create channel
+        if( (newstate.channelId === config.channels.pvtcreat && hasPremium) && !await LocalDb.userVoiceExist(newstate.id)){
+            console.log(`Criando o canal premium` + new Date())
+
+            const everyone = newstate.guild.roles.cache.get(newstate.guild.id)
+            const bots = newstate.guild.roles.cache.get(config.roles.bots)
+
+            const prmChannel = await newstate.guild.channels.create(`PV [${newstate.member.user.username}]`,{type:"GUILD_VOICE",parent: newstate.channel.parent,permissionOverwrites:[{id:newstate.member.id, allow:["MOVE_MEMBERS","CONNECT"],type:"member"},{id:everyone,deny:"CONNECT",allow:"STREAM",type:"role"}, {id:bots,allow:"CONNECT",type:"role"}]})
+            
+            LocalDb.attactivity(newstate.id,1)
+            LocalDb.savechan(prmChannel,newstate.id)
+            await newstate.setChannel(prmChannel)
+
+            }else if(await LocalDb.userVoiceExist(newstate.id)){
+            let channelUser = newstate.guild.channels.cache.get(await JSON.parse(JSON.stringify(await LocalDb.channelget(newstate.id))).id)
+            if(channelUser == newstate.channel){
+                LocalDb.attactivity(newstate.id,1)
+                clearTimeout(deletechan)
+            }
+        }
+    } catch (error) {
+        logger.error(error)
+    }
+    
+})
