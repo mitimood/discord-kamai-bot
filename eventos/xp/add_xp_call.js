8de8979ef403fbafbf4d3fbd0f025f4525561e69
mitimood <54547250+mitimood@users.noreply.github.com>
2022-02-10 19:21:39 -0300

    correção xp call

--- eventos/xp/add_xp_call.js
+++ eventos/xp/add_xp_call.js
@@ -7,14 +7,16 @@ const logger = require('../../utils/logger');
 var ids = []
 xp_voice_Add()
 
+const karoakeParentId = client.channels.cache.get(config.channels.equipekaraoke).parentId
+
 async function xp_voice_Add(){
     setTimeout(async ()=>{
         try {
             const voice = client.guilds.cache.get(config.guild_id).channels.cache.filter(channels => channels.isVoice())
             voice.forEach(voice_channel => {
                 if (voice_channel.members.size > 1 && voice_channel.members.filter(member => !member.bot).size >= 2) {
-                    voice_channel.members.forEach(member => {   
-                        if(!member.user.bot){
+                    voice_channel.members.forEach(member => {
+                        if((!member.user.bot && !member?.voice?.mute) || member?.voice?.channel?.parentId == karoakeParentId){
                             ids.push(member.id)
                         }
                     })
