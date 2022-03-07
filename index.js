d07e034566bcbc6f51e9a0643f7801cc5d9e4a48
mitimood <54547250+mitimood@users.noreply.github.com>
2022-03-07 13:37:18 -0300

    Update index.js

--- index.js
+++ index.js
@@ -115,6 +115,21 @@ client.on("ready", async () => {
 
     await gamesDB.MongoGamesClient.connect()
     
+    const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+    (async () => {
+        try {
+            console.log('Started refreshing application (/) commands.');
+
+            await rest.put(
+                Routes.applicationGuildCommands(client.application.id, config.guild_id),
+                { body: commands },
+            );
+
+            console.log('Successfully reloaded application (/) commands.');
+        } catch (error) {
+            console.error(error);
+        }
+    })();
 
     if (!client.application?.owner) await client.application?.fetch();
 
