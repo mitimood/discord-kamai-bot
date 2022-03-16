93443a2f37b271d90f63f0f0fdbb4c63cfd06175
mitimood <54547250+mitimood@users.noreply.github.com>
2022-03-15 22:19:25 -0300

    alteração avatar para gif

--- commands/commandpub/avatar.js
+++ commands/commandpub/avatar.js
@@ -15,8 +15,10 @@ module.exports={
 
             let userid = (msg.mentions.members.first()) ? msg.mentions.members.first().user.id : msgArgs[1]?.match(/[0-9]/) ? msgArgs[1] : msg.member.id;
             let member = await msg.guild.members.fetch( { user:userid, force: false } )
-            embed.setImage(member.user.displayAvatarURL( { size:2048, format: "png"  } ) )
+
+            embed.setImage(member.user.avatarURL({dynamic: true , format: 'png', size: 2048 }))
             embed.setColor(member.displayHexColor)
+            
             await msg.channel.send({content: msg.author.toString() , embeds: [embed]})
         } catch (error) {
             logger.error(error)
